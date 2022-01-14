0218fc23868ed4c399bda88ad14339692adac175
atkim2022 <atkim2022@chadwickschool.org>
2022-01-14 09:11:16 -0800

    part 1

--- Program.cs
+++ Program.cs
@@ -6,7 +6,41 @@ namespace atkim_DataEncodingInCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            char ch; 
+            ch = 'A'; 
+            Console.WriteLine($"ch is storing the value of {ch}");
+
+            int ch_decimal; 
+            ch_decimal = ch; 
+            Console.WriteLine($"The decimal value of ch is: {ch_decimal}");
+
+            int toConvert;
+            Console.Write("Enter an integer: ");
+            toConvert = int.Parse(Console.ReadLine());
+
+            char asChar;
+            asChar = (char)toConvert; 
+            Console.WriteLine($"The integer {toConvert} cast to a char is '{asChar}'");
+
+            char b;
+            b = (char)('A' + 1);
+            Console.WriteLine($"'A' + 1 = '{b}'");
+
+            char z;
+            z = (char)('Z' + 3);
+            Console.WriteLine($"'Z' + 3 = '{z}'");
+
+            string y;
+            // char e;
+            y = (string)("Khoor#Zruog$"); 
+            foreach (char e in y)
+            {
+                
+            }
+
+   
+           
+
         }
     }
 }
